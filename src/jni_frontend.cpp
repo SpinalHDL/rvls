cf9aef9c9b45af2f789504616a84b289ae2dc21d
Inochi Amaoto <inochiama@gmail.com>
2026-08-10 15:42:00 +0800

    Add asid parameters
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- src/jni_frontend.cpp
+++ src/jni_frontend.cpp
@@ -96,8 +96,8 @@ rvlsJni(newCpuMemoryView),int viewId, long readIds, long writeIds){
 	c->cpuMemoryViewNew(viewId, readIds, writeIds);
 }
 
-rvlsJni(newCpu),int hartId, jstring isa, jstring priv, int physWidth, int pmpNum, int triggerCount, int memoryViewId){
-	c->rvNew(hartId, toString(env, isa), toString(env, priv), physWidth, pmpNum, triggerCount, memoryViewId, c->spikeLogs);
+rvlsJni(newCpu),int hartId, jstring isa, jstring priv, int physWidth, int pmpNum, int triggerCount, int asidWidth, int memoryViewId){
+	c->rvNew(hartId, toString(env, isa), toString(env, priv), physWidth, pmpNum, triggerCount, asidWidth, memoryViewId, c->spikeLogs);
 }
 
 rvlsJni(loadElf), long offset, jstring path){
